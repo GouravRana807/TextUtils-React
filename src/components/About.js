d4897ea2f0ff512e1f2960f438b75458aa9047b1
Gourav Rana <85408869+GouravRana807@users.noreply.github.com>
2022-03-30 21:56:41 +0530

    Update About.js

--- src/components/About.js
+++ src/components/About.js
@@ -1,15 +1,11 @@
 import React from 'react';
 
 export default function About(props) {
-  // const [myStyle, setMyStyle] = useState({
-  //   color: 'black',
-  //   backgroundColor: 'white',
-  // });
+
   let myStyle = {
     color: props.mode === 'dark' ? 'white' : '#042743',
     backgroundColor: props.mode === 'dark' ? 'rgb(36 74 104)' : 'white',
-    // border: '2px solid',
-    // borderColor: props.mode === 'dark' ? 'white' : '#042743',
+    
   };
 
   return (
